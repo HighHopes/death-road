bb7e38f9e6000f805c886c81b562746e6d3a766e
High-Hopes <oprisaalin@gmail.com>
2020-02-11 02:33:29 +0200

    created RegistrationForm for User reg

---
+++ hero/users/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, ValidationError
+
+from hero.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=3, max=32)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = StringField("Password", validators=[DataRequired()])
+
+    submit = SubmitField("Sing Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+
+        if user:
+            raise ValidationError("Username is taken. Please choose another username.")
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+
+        if email:
+            raise ValidationError("E-mail is taken. Please choose another username.")
