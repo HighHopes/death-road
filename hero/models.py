27992cc0704554988ed71a6af5730a7acd9d4443
High-Hopes <oprisaalin@gmail.com>
2020-02-11 02:33:05 +0200

    created User class for registration and login

--- hero/models.py
+++ hero/models.py
@@ -6,3 +6,13 @@ class Subscription(db.Model):
 
     def __repr__(self):
         return "Email: {}".format(self.email)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return "Username: {}\nEmail: {}".format(self.username, self.email)
