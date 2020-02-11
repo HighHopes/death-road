f808e2d585d3b2fe9534c3ad0c028a9baf0e6648
High-Hopes <oprisaalin@gmail.com>
2020-02-11 02:33:43 +0200

    -

--- hero/main/forms.py
+++ hero/main/forms.py
@@ -1,4 +1,3 @@
-from flask import redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
