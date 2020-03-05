78122ba6a76c317a1b61c61aa8e88a87965e5c93
High-Hopes <oprisaalin@gmail.com>
2020-03-05 02:05:35 +0200

    -

--- hero/statistics/routes.py
+++ hero/statistics/routes.py
@@ -2,9 +2,14 @@ from hero import db
 from flask import Blueprint, render_template
 from flask_login import login_required
 
+from hero.models import User
+
 stats = Blueprint("stats", __name__)
+
 
 @stats.route("/statistics")
 @login_required
 def statistics():
-    return render_template("statistics.html", title="User statistics")
+    all_users = User.query.all()
+
+    return render_template("statistics.html", title="User statistics", all_users=all_users)
