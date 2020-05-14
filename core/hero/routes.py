20520aa85fc515b8357c12afbbc73059add596a9
HighHopes <oprisaalin@gmail.com>
2020-05-14 17:19:08 +0300

    -

--- core/hero/routes.py
+++ core/hero/routes.py
@@ -4,14 +4,14 @@ from flask_login import login_required, current_user
 from sqlalchemy.exc import IntegrityError
 
 from core import db
-from core.models import Hero
+from core.models import Hero, AnimalsTraining
 
 from core.hero.forms import CreateHeroForm, ReviveHeroForm
 from core.hero.utils import acc_has_hero, hp_regeneration, revive_hero
 
 from core.utils.unread_msgs import unread_msgs
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 hero = Blueprint("hero", __name__)
 
@@ -56,7 +56,11 @@ def hero_home():
         hero.alive = 1
         db.session.commit()
 
-    return render_template("hero_home.html", hero=hero, form=form, get_unread_msgs=get_unread_msgs)
+    # Time when the hero is alive again
+    reviving_time = hero.death_check + timedelta(seconds=10)
+
+    return render_template("hero_home.html", hero=hero, form=form, get_unread_msgs=get_unread_msgs,
+                           reviving_time=reviving_time)
 
 
 @hero.route("/hero/create", methods=["POST", "GET"])
@@ -145,4 +149,30 @@ def hero_training():
     if hero.alive == 1:
         revive_hero(current_user.id)
 
-    return render_template("hero_training.html", hero=hero, get_unread_msgs=get_unread_msgs)
+    # Time when the hero is alive again
+    reviving_time = hero.death_check + timedelta(seconds=10)
+    return render_template("hero_training.html", hero=hero, get_unread_msgs=get_unread_msgs, reviving_time=reviving_time)
+
+
+@hero.route("/hero/training/level_<int:id>")
+@login_required
+def train(id):
+    """
+    Training on different levels for different animals
+    """
+
+    # Check if the account has a hero created
+    # If FALSE redirect user to create a hero
+    if acc_has_hero(current_user.id):
+        return redirect(url_for("hero.hero_create"))
+
+    # Get the number of unread messages
+    get_unread_msgs = unread_msgs(current_user.username)
+
+    # Get the Hero from DB. Needed for stats page to list Hero hero vital infos
+    hero = Hero.query.filter_by(acc_id=current_user.id).first()
+
+    # Get the animal for the current training level
+    animal = AnimalsTraining.query.get_or_404(id).first()
+
+    return render_template("hero_train.html", animal=animal, get_unread_msgs=get_unread_msgs)
